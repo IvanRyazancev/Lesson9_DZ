b9b7053d181e7210b49302310d2697d8532c661c
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-02-14 21:47:29 +0300

    Задача №1. Показать натуральные числа от N до 1, N задано. Задача выполнена.

---
+++ Ex_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №1. Показать натуральные числа от N до 1, N задано.
+
+void PrintNums(int n)
+{    
+    if (n == 1) Console.WriteLine($"N: 1");
+    else
+    {        
+        Console.WriteLine($"N: {n}");
+        PrintNums(n - 1);
+    }
+}
+
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+PrintNums(n);
