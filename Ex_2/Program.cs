3a313fe94658af4899bf47f3f065ae24675f2d97
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-02-14 22:10:19 +0300

    Задача №2. Показать натуральные числа от M до N, N и M заданы. Задача выполнена.

---
+++ Ex_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №2. Показать натуральные числа от M до N, N и M заданы.
+
+void PrintNums(int X, int Y, int K)
+{    
+    if (X == Y) Console.WriteLine($"N: 1");
+    else if (X < Y && X <= K && K <= Y)
+    {         
+        Console.WriteLine($"M = : {K}");
+        PrintNums(X, Y, K + 1);        
+    }
+}
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int A = M;
+PrintNums(M, N, A);
